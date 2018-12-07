3a5ec598ca22f5ebb6e08b42a034f5228413a8f5
young_hustla <seena.zandipour@argus-lending.com>
2018-12-06 18:09:48 -0800

    Cleaned up & commented index.js

--- src/index.js
+++ src/index.js
@@ -1,98 +1,51 @@
-/* --------------------------------------------------- */
+//******************************************************************************/
 /*
-/*				RockBot App (Seena Remix)
-/*						12/04/2018
-/*	   		By: Seena Zandipour (craze3@gmail.com)
+/*						RockBot App (Seena Remix)
+/*								12/04/2018
+/*	   				By: Seena Zandipour (craze3@gmail.com)
 /*
-/* --------------------------------------------------- */
+//******************************************************************************/
 
-/*
-(function() {
-  'use strict';
-*/
+var app = angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
-  var app = angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-	
-	//******************************************************************************/
-	// API Polling Service
-	//******************************************************************************/
-	.factory('Poller', ['$http', '$timeout', function($http, $timeout) {
-	  var data = {};
-	  
-	  function poller(success){
-		$http.get('https://api.rockbot.com/v2/venue/10').then(function(r) {
-		  data = r.data;
-		  success(data);
-		  //console.log(data);
-		  $timeout(poller, 30000);
-		}).catch(function (err) {});
-	  };
-	  poller();
+//******************************************************************************/
+// API Polling Service
+//******************************************************************************/
+.factory('Poller', ['$http', '$timeout', function($http, $timeout) {
+	var data = {};
 
-	  return {
+	// Main polling function:
+	function poller(success) {
+		$http.get('https://api.rockbot.com/v2/venue/10').then(function(responseData) {
+			data = responseData.data;
+			success(data);
+			// Poll again in 30 seconds:
+			$timeout(function() {
+				poller(success);
+			}, 30000);
+		}).catch(function(err) {});
+	};
+
+	return {
 		poller: poller
-	  };
-	}])
+	};
+}])
 
-	//******************************************************************************/
-	// Main Controller
-	//******************************************************************************/
-	.controller('AppCtrl',['$scope', 'Poller', function($scope, Poller){
-		$scope.page = 1;
-		
-		//$scope.data = Poller.data;
-		Poller.poller(function(data) {
-			$scope.data = data;
-			console.log(data);
-		});
-			
-		var imagePath = "https://material.angularjs.org/latest/img/logo.svg";
-		 $scope.todos = [
-		  {
-			face : imagePath,
-			what: 'Brunch this weekend?',
-			who: 'Min Li Chan',
-			when: '3:08PM',
-			notes: " I'll be in your neighborhood doing errands"
-		  },
-		  {
-			face : imagePath,
-			what: 'Brunch this weekend?',
-			who: 'Min Li Chan',
-			when: '3:08PM',
-			notes: " I'll be in your neighborhood doing errands"
-		  },
-		  {
-			face : imagePath,
-			what: 'Brunch this weekend?',
-			who: 'Min Li Chan',
-			when: '3:08PM',
-			notes: " I'll be in your neighborhood doing errands"
-		  },
-		  {
-			face : imagePath,
-			what: 'Brunch this weekend?',
-			who: 'Min Li Chan',
-			when: '3:08PM',
-			notes: " I'll be in your neighborhood doing errands"
-		  },
-		  {
-			face : imagePath,
-			what: 'Brunch this weekend?',
-			who: 'Min Li Chan',
-			when: '3:08PM',
-			notes: " I'll be in your neighborhood doing errands"
-		  },
-		];
-		
-		$scope.currentNavItem = 'page1';
+//******************************************************************************/
+// Main Controller
+//******************************************************************************/
+.controller('AppCtrl', ['$scope', 'Poller', function($scope, Poller) {
+	$scope.page = 1;
 
-		$scope.goto = function(page) {
-			$scope.status = "Goto " + page;
-			$scope.page = page;
-		};
-		
-		
-	}]);
+	// Call the API polling service:
+	Poller.poller(function(data) {
+		// Update $scope.data, which automatically updates the UI:
+		$scope.data = data;
+		//console.log("Updated $scope.data! = ", $scope.data);
+	});
 
-//})();
+	// Function for switching between views:
+	$scope.goto = function(page) {
+		$scope.page = page;
+	};
+}]);
